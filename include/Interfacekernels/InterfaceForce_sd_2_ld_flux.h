e2c092b5672fecc47791d839dafbf89934a1b9f0
moose <zuzupsilon@gmail.com>
2019-06-16 08:30:33 -0400

    neglect metal reaction at interface

---
+++ include/Interfacekernels/InterfaceForce_sd_2_ld_flux.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "InterfaceKernel.h"
+#include "PostprocessorInterface.h"
+#include "VectorPostprocessorInterface.h"
+
+class InterfaceForce_sd_2_ld_flux;
+
+template<>
+InputParameters validParams<InterfaceForce_sd_2_ld_flux>();
+
+class InterfaceForce_sd_2_ld_flux :
+  public InterfaceKernel,
+  public PostprocessorInterface,
+  protected VectorPostprocessorInterface
+{
+public:
+  InterfaceForce_sd_2_ld_flux(const InputParameters & parameters);
+  virtual const PostprocessorValue & getPostprocessorValue(const std::string & name);
+  virtual const PostprocessorValue & getPostprocessorValueByName(const PostprocessorName & name);
+
+
+protected:
+  virtual Real computeQpResidual(Moose::DGResidualType type);
+  virtual Real computeQpJacobian(Moose::DGJacobianType type);
+
+  /// Depend UserObjects
+  std::set<std::string> _depend_uo;
+
+  const MaterialProperty<Real> & _driving_rate;
+
+  const MaterialProperty<Real> & _solubility_in_liquid;
+  const MaterialProperty<Real> & _solubility_in_solid;
+
+  const MaterialProperty<Real> & _diffusivity_in_liquid;
+  const MaterialProperty<Real> & _diffusivity_in_solid;
+
+};
